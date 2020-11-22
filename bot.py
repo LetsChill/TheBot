c61a6c3bdaf429dc086ca4f6395ae22bd144e7ef
LetsChill <74666116+LetsChill@users.noreply.github.com>
2020-11-22 16:39:33 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -138,22 +138,6 @@ async def giveaway(ctx):
     await channel.send(f"Congratulations! {winner.mention} won {prize}!")
 
 @client.command()
-@commands.has_role("Giveaways")
-async def reroll(ctx, channel : discord.TextChannel, id_ : int):
-    try:
-        new_msg = await channel.fetch_message(id_)
-    except:
-        await ctx.send("The id was entered incorrectly.")
-        return
-    
-    users = await new_msg.reactions[0].users().flatten()
-    users.pop(users.index(client.user))
-
-    winner = random.choice(users)
-
-    await channel.send(f"Congratulations! The new winner is {winner.mention}.!")
-
-@client.command()
 @commands.has_any_role("clear perms", "Admin")
 async def clear(ctx, amount=5):
     await ctx.channel.purge(limit=amount)
