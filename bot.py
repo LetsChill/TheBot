144e12349aa564b4abf7785befe3f3fa485cffb3
LetsChill <74666116+LetsChill@users.noreply.github.com>
2020-11-24 18:06:25 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,8 @@ from discord.ext import commands
 
 TOKEN = os.getenv("TOKEN")
 
-client = commands.Bot(command_prefix = ':')
+intents = discord.Intents().all()
+client = commands.Bot(command_prefix = ':', intents=intents)
 
 @client.event
 async def on_ready():
