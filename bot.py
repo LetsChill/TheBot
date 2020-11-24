db7fa2264f751baafd932074b39b9dfa5e46339b
LetsChill <74666116+LetsChill@users.noreply.github.com>
2020-11-24 14:39:28 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -144,7 +144,7 @@ async def clear(ctx, amount=5):
 
 @client.command()
 @commands.has_permissions(kick_members=True)
-async def kick(context, member, discord.Member):
+async def kick(context, member: discord.Member):
      
     await member.kick()
     await context.send('User ' + member.display_name + ' has been kicked')
