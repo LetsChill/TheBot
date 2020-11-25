01cc15479f7cb01406d66e77b13ca82536cc0d34
LetsChill <74666116+LetsChill@users.noreply.github.com>
2020-11-25 04:58:29 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -36,7 +36,7 @@ async def on_message_edit(before, after):
     await channel.send(embed=embed)
 
 @client.event
-Async def on_member_join(member)
+async def on_member_join(member)
     embed=discord.Embed(title=User joined:, color=0x982abc)
         embed.set_author(name=WonderBot,icon_url=https://cdn.discordapp.com/avatars/772894741582708778/db69e1a3b55e924eaf79dd3bccedebd7.png?size=128)
         embed.add_field(name=User:, value=f'{member.mention} (ID: {member.id}', inline=False)
