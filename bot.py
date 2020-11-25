aecf0ba8c72a0f10f693d51967eaf08952a7b2ab
LetsChill <74666116+LetsChill@users.noreply.github.com>
2020-11-25 03:43:16 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,18 +19,6 @@ async def on_ready():
 async def on_ready():
   await client.change_presence(status=discord.Status.idle, activity=discord.Game('Servers :help'))
  
-@client.event
-async def on_member_join(member):
-    emb = discord.Embed(
-        time.stamp=member.created_at,
-        color = discord.Colour(0x00FF00)
-        )
-    embed.set_author(name=f"{member.author.name} {member.author.discriminator}", icon_url=member.avatar_url)
-    embed.add_field(name="member joined", value=f"{member.mention} ID: {member.Id")
-    chat = client.get_channel(780760725299396629)
-    await chat.send(emb=embed)
-
-
 @client.command()
 @commands.has_any_role("clear perms", "Admin")
 async def clear(ctx, amount=5):
