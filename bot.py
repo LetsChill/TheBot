5813343cbccc452381eec923e2611e45d9173a60
LetsChill <74666116+LetsChill@users.noreply.github.com>
2020-11-25 04:03:09 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -42,7 +42,7 @@ async def on_member_join(member):
         color = discord.Colour(0x00FF00)
         )
     embed.set_author(name=f"{member.author.name} {member.author.discriminator}", icon_url=member.avatar_url)
-    embed.add_field(name="member joined", value=f"{member.mention} ID: {member.Id")
+    embed.add_field(name="member joined", value=f"{member.mention} ID: {member.Id}")
     chat = client.get_channel(780760725299396629)
     await chat.send(emb=embed)
 
