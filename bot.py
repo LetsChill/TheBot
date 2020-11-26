dfeb311124485849c9f8e4194c94c51f47d0b654
LetsChill <74666116+LetsChill@users.noreply.github.com>
2020-11-26 17:21:53 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,7 +22,7 @@ async def on_ready():
 
 @client.event
 async def on_ready():
-     await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="Emilia Suffer"))
+     await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="Emilia Suffer"))
 
 #--------------------------------Members logs----------------------------------
 @client.event
