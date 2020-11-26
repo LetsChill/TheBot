f64746f2f5e092ff280654288bc31f410c620f41
LetsChill <74666116+LetsChill@users.noreply.github.com>
2020-11-26 18:22:27 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -85,10 +85,17 @@ async def kick(context, member: discord.Member):
 @client.command()
 @commands.has_permissions(ban_members=True)
 async def ban(context, member: discord.Member):
+    banembed = discord.Embed(
+    title='User banned:', color=0x982abc
+    )
+    kickembed.set_author(name="WonderBot", icon_url=" https://cdn.discordapp.com/avatars/772894741582708778/db69e1a3b55e924eaf79dd3bccedebd7.png?size=128 ")
+    kickembed.add_field(name=' ', value=f'User: {member.mention} (ID: {member.id}) ', inline=False)
     channel = client.get_channel(780760958242652160)
+    
     await member.ban()
+    
     await context.send('User ' + member.mention + ' has been banned')
-
+    await channel.send(embed=banembed)
 #--------------------------------Music commands----------------------------------
 
 
