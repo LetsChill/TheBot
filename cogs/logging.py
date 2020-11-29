4ad081e6df09c7654a21c658cc237ddf29b1c5a5
LetsChill <74666116+LetsChill@users.noreply.github.com>
2020-11-29 06:57:33 +0200

    Update logging.py

--- cogs/logging.py
+++ cogs/logging.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 
 class Logging(commands.Cog):
 
-    def __init__(self, client)
+    def __init__(self, client):
         self.client = client
 
     @commands.cog.listener()
