c6cf544682e824d53b01e87e81ec3e95ec5fff9c
LetsChill <hahahamer2005@gmail.com>
2020-11-29 08:50:07 +0200

    indendint

--- cogs/moderator.py
+++ cogs/moderator.py
@@ -8,14 +8,14 @@ class Moderator(commands.Cog):
         self.client = client
 
 
-@commands.command()
-@commands.has_any_role("clear perms", "Admin")
-async def clear(self, ctx, amount=5):
-    await ctx.channel.purge(limit=amount)
+    @commands.command()
+    @commands.has_any_role("clear perms", "Admin")
+    async def clear(self, ctx, amount=5):
+        await ctx.channel.purge(limit=amount)
 
-@commands.command()
-@commands.has_permissions(kick_members=True)
-async def kick(self, context, member: discord.Member):
+    @commands.command()
+    @commands.has_permissions(kick_members=True)
+    async def kick(self, context, member: discord.Member):
     kickembed = discord.Embed(
     title='User kicked:', color=0x982abc
     )
@@ -23,15 +23,16 @@ async def kick(self, context, member: discord.Member):
     kickembed.add_field(name='User: ', value=f'{member.mention}(ID: {member.id}) ', inline=False)
     channel = self.client.get_channel(780760958242652160)
       
-    await member.kick()
+       await member.kick()
+                
+        
+       await context.send('User ' + member.mention + ' has been kicked')
+       await channel.send(embed=kickembed)
 
-    await context.send('User ' + member.mention + ' has been kicked')
-    await channel.send(embed=kickembed)
 
-
-@commands.command()
-@commands.has_permissions(ban_members=True)
-async def ban(self, context, member: discord.Member):
+    @commands.command()
+    @commands.has_permissions(ban_members=True)
+    async def ban(self, context, member: discord.Member):
     banembed = discord.Embed(
     title='User banned:', color=0x982abc
     )
@@ -39,13 +40,13 @@ async def ban(self, context, member: discord.Member):
     banembed.add_field(name=f'User: ', value=f'{member.mention} (ID: {member.id}) ', inline=False)
     channel = self.client.get_channel(780760958242652160)
     
-    await member.ban()
+        await member.ban()
     
-    await context.send('User ' + member.mention + ' has been banned')
-    await channel.send(embed=banembed)
+        await context.send('User ' + member.mention + ' has been banned')
+        await channel.send(embed=banembed)
 
-@commands.command()
-async def help(context):
+    @commands.command()
+    async def help(context):
     embhelp=discord.Embed(
     title="Commands availble", description="commands:", color=0x7a219e 
     )
@@ -59,7 +60,7 @@ async def help(context):
     embhelp.add_field(name="kick", value="kick a member ?kick [user] [reason]", inline=True)
     embhelp.add_field(name="clear", value="clear messages ?clear [value] default is 5 ", inline=True)
     embhelp.set_footer(text="Bot Coding by ChibiSubaru#2483")
-    await context.send(embed=embhelp)
+        await context.send(embed=embhelp)
 
 
 
