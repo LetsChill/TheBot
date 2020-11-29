021cf5569a6854dedab532940ddcb24576ca27cd
LetsChill <74666116+LetsChill@users.noreply.github.com>
2020-11-29 06:53:11 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,52 +22,6 @@ async def on_ready():
      await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="Emilia Suffer"))
 
 #--------------------------------Members logs----------------------------------
-@client.event
-async def on_message_edit(before, after):
-    embed = discord.Embed(
-        timestamp=after.created_at,
-        description = "message was edited", color=0x982abc
-        ) 
-    embed.set_author(name=f'{before.author.name} {before.author.discriminator}', icon_url=before.author.avatar_url)
-    embed.set_footer(text=f"Author ID:{before.author.id} • Message ID: {before.id}")
-    embed.add_field(name=f'User: ', value=f'{before.author.mention}', inline=False)
-    embed.add_field(name='Before:', value=before.content, inline=False)
-    embed.add_field(name="After:", value=after.content, inline=False)
-    channel = client.get_channel(780760893125689364)
-    await channel.send(embed=embed)
-
-@client.event
-async def on_message_delete(message):
-  
-   embdel = discord.Embed(
-        title='User Message deleted', color=0x982abc
-     	)
-   embdel.set_author(name='Subaru')
-   embdel.add_field(name=f'User:', value=f'{message.author.mention}')
-   embdel.add_field(name='message deleted', value=message.content)
-   channel = client.get_channel(780760893125689364)
-   await channel.send(embed=embdel)
-
-@client.event
-async def on_member_join(member):
-    embmsg = discord.Embed(
-        title='User joined:', color=0x982abc
-        )
-    embmsg.set_author(name="WonderBot")
-    embmsg.add_field(name='User: ', value=f'{member.mention}(ID: {member.id})', inline=False)
-    channel = client.get_channel(780760725299396629)
-    await channel.send(embed=embmsg)
-
-@client.event
-async def on_member_remove(member):
-    embmsg1 = discord.Embed(
-        title='User left:', color=0x982abc
-        )
-    embmsg1.set_author(name="WonderBot")
-    embmsg1.add_field(name=f'User: ', value=f'{member.mention} (ID: {member.id})', inline=False)
-    channel = client.get_channel(780760725299396629)
-    await channel.send(embed=embmsg1)
-#--------------------------------Members logs----------------------------------
 
 @client.command()
 @commands.has_any_role("clear perms", "Admin")
@@ -122,6 +76,10 @@ async def help(context):
     embhelp.set_footer(text="Bot Coding by ChibiSubaru#2483")
     await context.send(embed=embhelp)
 #--------------------------------Music commands----------------------------------                           
+
+for filename in os.listdir('./cogs'):
+    if filename.endwith('.py'):
+       client.load_extension(f'cogs.{filename[:-3]}')
 
 if __name__ == "__main__":
     client.run(TOKEN)
