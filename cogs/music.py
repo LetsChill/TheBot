28c6cce3ebfe81910d197e1e3da56798d0b86de5
LetsChill <hahahamer2005@gmail.com>
2020-11-30 06:55:30 +0200

    changed invalid characters

--- cogs/music.py
+++ cogs/music.py
@@ -60,7 +60,7 @@ class Music(commands.Cog):
     @commands.command()
     async def queue(self, ctx):
         player = music.get_player(guild_id=ctx.guild.id)
-        await ctx.send(f"{‘, ‘.join([song.name for song in player.current_queue()])}")
+        await ctx.send(f"{', '.join([song.name for song in player.current_queue()])}")
 
     @commands.command()
     async def np(self, ctx):
