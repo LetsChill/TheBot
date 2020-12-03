884d9af21ac580b1514f06522fbeba73d99f921c
LetsChill <hahahamer2005@gmail.com>
2020-12-03 11:05:21 +0200

    the stupid self

--- cogs/text.py
+++ cogs/text.py
@@ -6,7 +6,7 @@ class Text(commands.Cog):
     def __init__(self, client):
         self.client = client
         
-    @commands.command(self, ctx)
+    @commands.command(ctx)
     async def donate():
         dembed=discord.Embed(
         title="Donate me!", description="http://paypal.me/LetsChiLI", color=0x7a219e 
