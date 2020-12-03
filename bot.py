1868c45d32eade15e5b69fad7f9235defbe74e08
LetsChill <hahahamer2005@gmail.com>
2020-12-03 19:22:14 +0200

    i messed up

--- bot.py
+++ bot.py
@@ -22,7 +22,6 @@ async def on_ready():
 async def on_ready():
      await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="Under Development in this moment")
                  
-                 
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
        client.load_extension(f'cogs.{filename[:-3]}')
