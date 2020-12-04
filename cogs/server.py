3389f7cfb30adb61b6dab825caac91458fcc0ccd
LetsChill <hahahamer2005@gmail.com>
2020-12-04 22:01:58 +0200

    lets se

---
+++ cogs/server.py
@@ -0,0 +1,15 @@
+class Server(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+    
+    
+    @commands.command()
+    async def create(self, ctx)
+        await ctx.send("creating the server atm")
+        await  guild.create_category("Text Channels")
+        
+        await guild.create_text_channel("general┃💭", category="Text Channels")
+        
+def setup(client):
+    client.add_cog(Server(client))
