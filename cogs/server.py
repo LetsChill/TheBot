78bb4985ad06634bd604b95b09971484cb18f7a5
LetsChill <hahahamer2005@gmail.com>
2020-12-05 05:19:38 +0200

    hello

--- cogs/server.py
+++ cogs/server.py
@@ -11,9 +11,30 @@ class Server(commands.Cog):
     @commands.command()
     async def create(self, ctx):
         guild = ctx.guild
-        category = await guild.create_category("Text Channel")
-        await ctx.send("creating the server atm")
-        await guild.create_text_channel("general┃💭", category=category)
+        ancat = await guild.create_category("Welcome")
+        
+        txtcat = await guild.create_category("text channels")
+        
+        givcat = await guild.create_category("giveaways")
+        
+        stfcat = await guild.create_category("staff chat")
+        
+        await ctx.send("creating the server at the moment!")
+        
+        await guild.create_text_channel("general┃💭", category=txtcat)
+        await guild.create_text_channel("check-invites", category=txtcat)
+        
+        await guild.create_text_channel("invite-tracker", category=ancat)
+        await guild.create_text_channel("announcements", category=ancat)
+        
+        await guild.create_text_channel("giveaway-1", category=givcat)
+        await guild.create_text_channel("giveaway-2", category=givcat)
+        await guild.create_text_channel("giveaway-3", category=givcat)
+        await guild.create_text_channel("proofs", category=givcat)
+        
+        await guild.create_text_channel("staff-chat", category=stfcat)
+        await guild.create_text_channel("staff-commands", category=stfcat)
+        await guild.create_text_channel("accounts", category=stfcat)
         
 def setup(client):
     client.add_cog(Server(client))
