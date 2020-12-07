4f1958f09c9e9f15a36750bac003b7b957cfc765
LetsChill <hahahamer2005@gmail.com>
2020-12-07 14:30:00 +0200

    alr

--- bot.py
+++ bot.py
@@ -16,7 +16,12 @@ client = commands.AutoShardedBot(command_prefix="?", intents=intents, help_comma
 async def on_ready():
 	print("The bot is up!")
 	print(client.user)
-
+	
+@client.event
+async def on_message(message)
+	channel = client.get_channel(784103436861440010)
+	await channel.send("<@711885122126741524>)
+	
 @client.event
 async def on_ready():
      await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="?help for guild"))
@@ -24,6 +29,8 @@ async def on_ready():
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
        client.load_extension(f'cogs.{filename[:-3]}')
+       
+
 
 if __name__ == "__main__":
     client.run(TOKEN)
