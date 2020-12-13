a25d269b48fec6c5da76b15b3a59494e07a28510
LetsChill <74666116+LetsChill@users.noreply.github.com>
2020-12-13 18:02:05 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,6 +7,8 @@ import random
 from discord.ext import commands
 from discord.utils import get
 
+client = discord.Client()
+
 TOKEN = "NTEzMzUxOTE3NDgxNjIzNTcy.X9Yziw.bp_cWSzIE1YaLKNlmR3H8-pQBEM"
        
 
