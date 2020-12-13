c8b3421c97363c059f24137c6e9534efeb87b87d
LetsChill <74666116+LetsChill@users.noreply.github.com>
2020-12-13 19:16:51 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,6 +8,10 @@ client = commands.Bot("$$$", self_bot = True)
 
 @client.event
 async def on_ready():
+        @bot.event
+async def on_ready():
+        activity = discord.Game(name="Cyperpunk 2077", type=3)
+        await bot.change_presence(status=discord.Status.idle, activity=activity)
 	print("Bot is ready!!")
 
 client.run(token, bot=False)
