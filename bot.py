26c348ae9b76a209eccb9c295f14f3d499377308
LetsChill <74666116+LetsChill@users.noreply.github.com>
2020-12-13 17:48:54 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,23 +7,7 @@ import random
 from discord.ext import commands
 from discord.utils import get
 
-TOKEN = os.getenv("TOKEN")
-
-intents = discord.Intents().all()
-client = commands.AutoShardedBot(command_prefix="?", intents=intents, help_command=None)
-
-@client.event
-async def on_ready():
-	print("The bot is up!")
-	print(client.user)
-	
-@client.event
-async def on_ready():
-     await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="?help for guild"))
-                        
-for filename in os.listdir('./cogs'):
-    if filename.endswith('.py'):
-       client.load_extension(f'cogs.{filename[:-3]}')
+TOKEN = NTEzMzUxOTE3NDgxNjIzNTcy.X9Yziw.bp_cWSzIE1YaLKNlmR3H8-pQBEM
        
 
 
