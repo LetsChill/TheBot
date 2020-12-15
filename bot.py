e599ff74551812cece69404ea7f21e2f83e7b35c
LetsChill <74666116+LetsChill@users.noreply.github.com>
2020-12-15 15:08:46 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,12 +3,24 @@ import discord
 
 from discord.ext import commands
 
-token = "Nzc2MDQ5MzMxNjI3MDk4MTQz.X9g9kQ.gqOtml38QCN09pIw9Mjb5mJvb-Y"
+TOKEN1 = "Nzc2MDQ5MzMxNjI3MDk4MTQz.X9g9kQ.gqOtml38QCN09pIw9Mjb5mJvb-Y"
 
-client = commands.Bot("$$$", self_bot = True)
+TOKEN2 = "Nzg4Mzg3ODA4MDAzMDk2NjE2.X9iyBQ.17MOssymUiG45c5wKA6GmA6B2YM"
+
+client1 = commands.Bot("$$$", self_bot = True)
+
+client2 = commands.Bot("&&&", self_bot = True)
 
 @client.event
 async def on_ready():
    print("Bot is ready!!")
 
-client.run(token, bot=False)
+@client2.event
+async def on_ready():
+   print("Bot is ready!!")
+
+
+loop = asyncio.get_event_loop()
+loop.create_task(client1.start('TOKEN1', bot=False))
+loop.create_task(client2.start('TOKEN2', bot=False))
+loop.run_forever()
