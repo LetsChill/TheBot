cd0b3fa8fbd09dd01d559e1cb57d23dc7747fe9c
LetsChill <74666116+LetsChill@users.noreply.github.com>
2020-12-15 07:24:09 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,9 +7,16 @@ from discord.ext import commands
 
 token = "Nzc2MDQ5MzMxNjI3MDk4MTQz.X9g9kQ.gqOtml38QCN09pIw9Mjb5mJvb-Y"
 
+token2 = "Nzc2MDQ1MzE0MzIyNTk1ODYw.X9hHGQ.e1Pd1nh5OHzdNNAVjUGEsIJ44fI"
 client = commands.Bot("$$$", self_bot = True)
 
+client2 = commands.Bot("$$$", self_bot = True)
+
 @client.event
+async def on_ready():
+   print("Bot is ready!!")
+
+@client2.event
 async def on_ready():
    print("Bot is ready!!")
 
@@ -18,3 +25,4 @@ async def joinserver(mahlink):
     await client.accept_invite(mahlink)
 
 client.run(token, bot=False)
+client2.run(token2, bot=False)
