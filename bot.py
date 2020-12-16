5cb05a8326e7673d97a7324fd5c2d727dad3d652
LetsChill <74666116+LetsChill@users.noreply.github.com>
2020-12-16 18:05:54 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -39,7 +39,7 @@ TOKEN15 = "Nzg4Nzk0NDQ3Mzc0MTg4NTc1.X9osvQ.bncVRtfIqr6HoDdlOlyGHNn-9NI"
 
 #JOINER, LEAVER!!!
 
-TARGET = "https://discordapp.com/api/v6/invites/kcFd6ztN"
+TARGET = "https://discordapp.com/api/v6/invites/QURbjRXqn7"
 
 LEAVE = 742618169419628554
 
