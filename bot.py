2894a564afbcc0f23374bbf14eae823ec34c09ae
LetsChill <74666116+LetsChill@users.noreply.github.com>
2020-12-16 20:06:07 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -39,7 +39,7 @@ TOKEN15 = "Nzg4Nzk0NDQ3Mzc0MTg4NTc1.X9osvQ.bncVRtfIqr6HoDdlOlyGHNn-9NI"
 
 #JOINER, LEAVER!!!
 
-TARGET = "https://discordapp.com/api/v6/invites/"
+TARGET = "https://discordapp.com/api/v6/invites/depjxkE9de"
 
 LEAVE = 784444505625329704
 
