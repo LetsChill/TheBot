1a4fb332eb25c928aba5316108115e1b403a02d2
LetsChill <74666116+LetsChill@users.noreply.github.com>
2020-12-16 19:50:56 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -41,7 +41,7 @@ TOKEN15 = "Nzg4Nzk0NDQ3Mzc0MTg4NTc1.X9osvQ.bncVRtfIqr6HoDdlOlyGHNn-9NI"
 
 TARGET = "https://discordapp.com/api/v6/invites/QURbjRXqn7"
 
-LEAVE = 742618169419628554
+LEAVE = 784444505625329704
 
 #CLIENT DECLARES
 
@@ -103,7 +103,7 @@ pfp4 = fp4.read()
 async def on_ready():
    SERVER = client1.get_guild(LEAVE)
    requests.post(TARGET,headers={'authorization':TOKEN1})
-   #await SERVER.leave()
+   await SERVER.leave()
    print(f"Bot 1 is ready!!. Login as {client1.user.name}")
    
 @client2.event
@@ -111,14 +111,14 @@ async def on_ready():
    SERVER = client2.get_guild(LEAVE)
    #await client2.user.edit(avatar=pfp1)
    requests.post(TARGET,headers={'authorization':TOKEN2})
-   #await SERVER.leave()
+   await SERVER.leave()
    print(f"Bot 2 is ready!!. Login as {client2.user.name}")
 
 @client3.event
 async def on_ready():
    SERVER = client3.get_guild(LEAVE)
    requests.post(TARGET,headers={'authorization':TOKEN3})
-   #await SERVER.leave()
+   await SERVER.leave()
    print(f"Bot 3 is ready!!. Login as {client3.user.name}")
 
 @client4.event
@@ -126,7 +126,7 @@ async def on_ready():
    SERVER = client4.get_guild(LEAVE)
    #await client4.user.edit(avatar=pfp2)
    requests.post(TARGET,headers={'authorization':TOKEN4})
-   #await SERVER.leave()
+   await SERVER.leave()
    print(f"Bot 4 is ready!!. Login as {client4.user.name}")
 
 @client5.event
@@ -141,7 +141,7 @@ async def on_ready():
    SERVER = client6.get_guild(LEAVE)
    #await client6.user.edit(avatar=pfp3)
    requests.post(TARGET,headers={'authorization':TOKEN6})
-   #await SERVER.leave()
+   await SERVER.leave()
    print(f"Bot 6 is ready!!. Login as {client6.user.name}")
 
 @client7.event
@@ -149,28 +149,28 @@ async def on_ready():
    SERVER = client7.get_guild(LEAVE)
    #await client7.user.edit(avatar=pfp4)
    requests.post(TARGET,headers={'authorization':TOKEN7})
-   #await SERVER.leave()
+   await SERVER.leave()
    print(f"Bot 7 is ready!!. Login as {client7.user.name}")
 
 @client8.event
 async def on_ready():
    SERVER = client8.get_guild(LEAVE)
    requests.post(TARGET,headers={'authorization':TOKEN8})
-   #await SERVER.leave()
+   await SERVER.leave()
    print(f"Bot 8 is ready!!. Login as {client8.user.name}")
 
 @client9.event
 async def on_ready():
    SERVER = client9.get_guild(LEAVE)
    requests.post(TARGET,headers={'authorization':TOKEN9})
-   #await SERVER.leave()
+   await SERVER.leave()
    print(f"Bot 9 is ready!!. Login as {client9.user.name}")
 
 @clientX.event
 async def on_ready():
    SERVER = clientX.get_guild(LEAVE)
    requests.post(TARGET,headers={'authorization':TOKENX})
-   #await SERVER.leave()
+   await SERVER.leave()
    print(f"Bot X is ready!!. Login as {clientX.user.name}")
 
 @client11.event
