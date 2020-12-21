701a3686e261990b058256308a1c464b9582a840
LetsChill <74666116+LetsChill@users.noreply.github.com>
2020-12-21 08:07:33 +0200

    Update mine.py

--- mine.py
+++ mine.py
@@ -2,12 +2,12 @@ import discord
 
 client = discord.Client()
 
-TOKEN = "NTEzMzUxOTE3NDgxNjIzNTcy.X9Yziw.bp_cWSzIE1YaLKNlmR3H8-pQBE"
+TOKEN = "NTEzMzUxOTE3NDgxNjIzNTcy.X-A6lQ.w15VyhN5ffRGikxDpA0HgbY4orc"
 
 
 @client.event
 async def on_ready():
-    activity=discord.Game(name="a game")
+    activity=discord.Game(name="Test.py.exe")
     await client.change_presence(status=discord.Status.idle, activity=activity)
     print(f"ready! {client.user.name}")
 
