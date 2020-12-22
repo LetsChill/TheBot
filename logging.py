5a9fd188fa5d4e64b5b7b3f4c0a35713a5931f4c
LetsChill <74666116+LetsChill@users.noreply.github.com>
2020-12-22 12:28:48 +0200

    Add files via upload

---
+++ logging.py
@@ -0,0 +1,61 @@
+import discord
+
+from discord.ext import commands
+
+from discord.utils import get
+
+class Logging(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    @commands.Cog.listener()
+    async def on_message_edit(self, before, after):
+        embed = discord.Embed(
+           
+        timestamp=after.created_at,
+        description = "message was edited", color=0x982abc
+        ) 
+        embed.set_author(name=f'{before.author.name} {before.author.discriminator}', icon_url=before.author.avatar_url)
+        embed.set_footer(text=f"Author ID:{before.author.id} • Message ID: {before.id}")
+        embed.add_field(name=f'User: ', value=f'{before.author.mention}', inline=False)
+        embed.add_field(name='Before:', value=before.content, inline=False)
+        embed.add_field(name="After:", value=after.content, inline=False)
+        channel = discord.utils.get(self.client.get_all_channels(), name='message-logs')
+        await channel.send(embed=embed)
+
+    @commands.Cog.listener()
+    async def on_message_delete(self, message):
+        embdel = discord.Embed(
+        title='User Message deleted', color=0x982abc
+        )
+        embdel.set_author(name='Subaru')
+        embdel.add_field(name=f'User:', value=f'{message.author.mention}')
+        embdel.add_field(name='message deleted', value=message.content)
+        channel = discord.utils.get(self.client.get_all_channels(), name='message-logs')
+        await channel.send(embed=embdel)
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        count = discord.utils.get(self.client.get_all_channels(), name='members:')
+        embmsg = discord.Embed(
+        title='User joined:', color=0x982abc
+        )
+        embmsg.set_author(name="Subaru")
+        embmsg.add_field(name='User: ', value=f"{member.mention}(ID: {member.id}) account age: {member.created_at}", inline=False)
+        channel = discord.utils.get(self.client.get_all_channels(), name='member-logs')
+        await VoiceChannel.edit(name=f"members: {guild.member_count}")
+        await channel.send(embed=embmsg)
+
+    @commands.Cog.listener()
+    async def on_member_remove(self, member):
+        embmsg1 = discord.Embed(
+        title='User left:', color=0x982abc
+        )
+        embmsg1.set_author(name="Subaru")
+        embmsg1.add_field(name=f'User: ', value=f'{member.mention} (ID: {member.id})', inline=False)
+        channel = discord.utils.get(self.client.get_all_channels(), name='member-logs')
+        await channel.send(embed=embmsg1)
+
+def setup(client):
+    client.add_cog(Logging(client))
---
+++ moderator.py
@@ -0,0 +1,73 @@
+import discord
+
+from discord.ext import commands
+
+class Moderator(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+
+    @commands.command()
+    @commands.has_any_role("clear perms", "Admin")
+    async def clear(self, ctx, amount=1):
+        await ctx.channel.purge(limit=1)
+        await ctx.channel.purge(limit=amount)
+        await ctx.channel.send(f"purged {amount} messages", delete_after=4)
+
+    @commands.command()
+    @commands.has_permissions(kick_members=True)
+    async def kick(self, context, member: discord.Member):
+        kickembed = discord.Embed(
+        title='User kicked:', color=0x982abc
+        )
+        kickembed.set_author(name="WonderBot", icon_url=" https://cdn.discordapp.com/avatars/772894741582708778/db69e1a3b55e924eaf79dd3bccedebd7.png?size=128 ")
+        kickembed.add_field(name='User: ', value=f'{member.mention}(ID: {member.id}) ', inline=False)
+        channel = self.client.get_channel(784105806135296042)
+        await member.kick()
+                
+        
+        await context.send('User ' + member.mention + ' has been kicked')
+        await channel.send(embed=kickembed)
+
+
+    @commands.command()
+    @commands.has_permissions(ban_members=True)
+    async def ban(self, context, member: discord.Member):
+        banembed = discord.Embed(
+        title='User banned:', color=0x982abc
+        )
+        banembed.set_author(name="WonderBot", icon_url=" https://cdn.discordapp.com/avatars/772894741582708778/db69e1a3b55e924eaf79dd3bccedebd7.png?size=128 ")
+        banembed.add_field(name=f'User: ', value=f'{member.mention} (ID: {member.id}) ', inline=False)
+        channel = self.client.get_channel(784105806135296042)
+    
+        await member.ban()
+    
+        await context.send('User ' + member.mention + ' has been banned')
+        await channel.send(embed=banembed)
+
+    @commands.command()
+    async def help(self, context):
+        embhelp=discord.Embed(
+        title="Commands availble", description="commands:", color=0x7a219e 
+        )
+        embhelp.set_author(name="Costum Bot 1.1v")
+        embhelp.add_field(name="help", value="Shows commands availble", inline=False)
+        embhelp.add_field(name="play", value="?play [url] or ?play [search engine]", inline=True)
+        embhelp.add_field(name="stop", value="stop the music and makes the bot leaves the channel", inline=True)
+        embhelp.add_field(name="join", value="join a voice channel with [ID] of the channel", inline=True)
+        embhelp.add_field(name="leave", value="makes the bot leaves the channel", inline=True)
+        embhelp.add_field(name="queue", value="shows tye queue of songs will be played in future", inline=True)
+        embhelp.add_field(name="remove", value="?remove [order number in the queue], to remove any playlist from the queue", inline=True)
+        embhelp.add_field(name="donate", value="Make my Day amazing and buy me a cake!", inline=True)
+        embhelp.set_footer(text="Bot Coding by HАJякя#2483")
+        await context.send(embed=embhelp)
+
+
+
+
+def setup(client):
+    client.add_cog(Moderator(client))
+
+
+
