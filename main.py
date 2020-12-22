2ef356ab3d33a5bb3c457d986cf36a2ebc89c074
LetsChill <74666116+LetsChill@users.noreply.github.com>
2020-12-22 20:28:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,11 +11,11 @@ from discord.ext import commands
 from discord.utils import get
 
 intents = discord.Intents().all()
-client = commands.AutoShardedBot(command_prefix="?", intents=intents, help_command=None)
+client = commands.AutoShardedBot(command_prefix=":", intents=intents, help_command=None)
 	
 @client.event
 async def on_ready():
-     await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="Humans, ?help"))
+     await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="Humans, :help"))
      print(f"Bot Is Up {client.user.name}")
                         
 for filename in os.listdir('./cogs'):
