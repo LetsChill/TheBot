f53996fd6435d38390e88819dbeadb4bad86e69e
LetsChill <74666116+LetsChill@users.noreply.github.com>
2020-12-23 14:56:41 +0200

    Update info.py

--- cogs/info.py
+++ cogs/info.py
@@ -39,15 +39,14 @@ class Info(commands.Cog):
               embhelp=discord.Embed(
               title="Commands availble", description="commands:", color=0x7a219e 
               )
-              embhelp.set_author(name="Costum Bot 1.1v, *prefix is* **:**")
-              embhelp.add_field(name="help", value="Shows commands availble", inline=False)
+              embhelp.set_author(name="Subary Bot 2.0v, *prefix is* :")
               embhelp.add_field(name="play", value="play [url] or play [search engine]", inline=True)
               embhelp.add_field(name="stop", value="stop the music and makes the bot leaves the channel", inline=True)
               embhelp.add_field(name="join", value="join a voice channel with [ID] of the channel", inline=True)
               embhelp.add_field(name="leave", value="makes the bot leaves the channel", inline=True)
               embhelp.add_field(name="queue", value="shows tye queue of songs will be played in future", inline=True)
               embhelp.add_field(name="remove", value="remove [order number in the queue], to remove any playlist from the queue", inline=True)
-              embhelp.add_field(name="", value="Make my Day amazing and buy me a cake!", inline=True)
+              embhelp.add_field(name="", value="", inline=True)
               embhelp.set_footer(text="Bot Coding by HАJякя#2483")
               await ctx.send(embed=embhelp)
 
@@ -61,14 +60,15 @@ class Info(commands.Cog):
               embhelp2.add_field(name="ban", value="ban [mention] only to people who have ban perms, be sure to put the bot role above members, if its above staff, it can ban them!", inline=True)
               await ctx.send(embed=embhelp2)
 
-         elif isinstance(error, discord.ext.commands.MissingRequiredArgument):
+         elif arg1 == None:
               embhelp3=discord.Embed(
               title="Commands availble", description="commands:", color=0x7a219e 
               )
-              embhelp3.set_author(name="Costum Bot 1.1v, *prefix is :")
+              embhelp3.set_author(name="Subary Bot 2.0v, prefix is :")
               embhelp3.add_field(name="help Music", value="Music commands help!", inline=False)
               embhelp3.add_field(name="help mod", value="Moderation commands help!", inline=False)
               await ctx.send(embed=embhelp3)
+
          else:
               embhelp4=discord.Embed(
               title="invalid Argument", description="Invalid Argument was passed", color=0x7a219e
