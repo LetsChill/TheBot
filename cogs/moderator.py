4924f1bab8aa0e64810a7f3ec4d5003e46d8ecf6
LetsChill <74666116+LetsChill@users.noreply.github.com>
2020-12-23 06:14:53 +0200

    Update moderator.py

--- cogs/moderator.py
+++ cogs/moderator.py
@@ -47,25 +47,6 @@ class Moderator(commands.Cog):
     
         await context.send('User ' + member.mention + ' has been banned')
         await channel.send(embed=banembed)
-
-    @commands.command()
-    async def help(self, context):
-        embhelp=discord.Embed(
-        title="Commands availble", description="commands:", color=0x7a219e 
-        )
-        embhelp.set_author(name="Costum Bot 1.1v")
-        embhelp.add_field(name="help", value="Shows commands availble", inline=False)
-        embhelp.add_field(name="play", value="?play [url] or ?play [search engine]", inline=True)
-        embhelp.add_field(name="stop", value="stop the music and makes the bot leaves the channel", inline=True)
-        embhelp.add_field(name="join", value="join a voice channel of the channel", inline=True)
-        embhelp.add_field(name="leave", value="makes the bot leaves the channel", inline=True)
-        embhelp.add_field(name="queue", value="shows tye queue of songs will be played in future", inline=True)
-        embhelp.add_field(name="remove", value="?remove [order number in the queue], to remove any playlist from the queue", inline=True)
-        embhelp.add_field(name="setup", value="server owner and admin only!, set up the logging system!", inline=True)
-        embhelp.add_field(name="Lost?", value="Join our server! https://invite.subaru.cf/", inline=True)
-        embhelp.set_footer(text="Bot Coding by LetsChill#0001")
-        await context.send(embed=embhelp)
-        
         
 
     @commands.command()
