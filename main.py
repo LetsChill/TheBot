74c9481f4c11abbc479929ed1382801c1d6ab6bf
LetsChill <74666116+LetsChill@users.noreply.github.com>
2020-12-24 09:52:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,21 +12,11 @@ from discord.utils import get
 
 intents = discord.Intents().all()
 client = commands.AutoShardedBot(command_prefix=":", intents=intents, help_command=None)
-	
-client.change_stats.start()
 
-client.change_stats2.start()
-
-@tasks.loop(seconds=30.0)
-async def change_stats():
+@client.event()
+async def on_member_join(member):
      await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"Humans on {len(client.guilds)} Servers , :help"))
-     print(f"Bot Is Up {client.user.name}")
-
-@tasks.loop(seconds=60.0)
-async def change_stats2():
-     await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"invite.subary.cf ! Visit us!"))
-     print(f"Is looping! {client.user.name}")
-
+     print(f"Tada! {member.user.name}")
                         
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
