665b5df14bd8752c32ce614e0675f838b77734b5
LetsChill <74666116+LetsChill@users.noreply.github.com>
2020-12-25 09:13:55 +0200

    Update moderator.py

--- cogs/moderator.py
+++ cogs/moderator.py
@@ -12,15 +12,15 @@ class Moderator(commands.Cog):
     @commands.command()
     @commands.has_permissions(manage_messages=True)
     async def clear(self, ctx, amount=1):
-        await ctx.channel.purge(limit=1)
-        await ctx.channel.purge(limit=amount)
-        await ctx.channel.send(f"purged {amount} messages", delete_after=4)
+         await ctx.channel.purge(limit=1)
+         await ctx.channel.purge(limit=amount)
+         await ctx.channel.send(f"purged {amount} messages", delete_after=4)
 
     @commands.command()
     @commands.has_permissions(kick_members=True)
     async def kick(self, context, member: discord.Member):
        if member.guild_permissions.kick_members:
-           await context.send("You cant kick A mod/admin")
+            await context.send("You cant kick A mod/admin")
        else:
             guild = context.guild
             kickembed = discord.Embed(
@@ -40,31 +40,20 @@ class Moderator(commands.Cog):
     @commands.command()
     @commands.has_permissions(ban_members=True)
     async def ban(self, context, member: discord.Member):
-        guild = context.guild
-        banembed = discord.Embed(
-        title='User banned:', color=0x982abc
-        )
-        banembed.set_author(name="WonderBot", icon_url=" https://cdn.discordapp.com/avatars/772894741582708778/db69e1a3b55e924eaf79dd3bccedebd7.png?size=128 ")
-        banembed.add_field(name=f'User: ', value=f'{member.mention} (ID: {member.id}) ', inline=False)
-        channel = discord.utils.get(guild.channels, name='mod-logs')
-        await member.ban()
+       if member.guild_permissions.ban_members:
+            await context.send("you cant ban a mod/admin")
+       else:
+            guild = context.guild
+            banembed = discord.Embed(
+            title='User banned:', color=0x982abc
+            )
+            banembed.set_author(name="WonderBot", icon_url=" https://cdn.discordapp.com/avatars/772894741582708778/db69e1a3b55e924eaf79dd3bccedebd7.png?size=128 ")
+            banembed.add_field(name=f'User: ', value=f'{member.mention} (ID: {member.id}) ', inline=False)
+            channel = discord.utils.get(guild.channels, name='mod-logs')
+            await member.ban()
     
-        await context.send('User ' + member.mention + ' has been banned')
-        await channel.send(embed=banembed)
-        
-
-    @commands.command()
-    async def copy(self, ctx):
-      with open("messages.txt", "w") as f:
-          async for message in ctx.history(limit=1000):
-              f.write(message.content + "\n")
-
-      await ctx.send("Done! copied 1k messages!")
-      
-        
-
-
-
+            await context.send('User ' + member.mention + ' has been banned')
+            await channel.send(embed=banembed)
 
 def setup(client):
     client.add_cog(Moderator(client))
