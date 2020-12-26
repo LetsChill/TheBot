2ca8a8af8ae71a72d81448e4008f404b0c365a09
LetsChill <74666116+LetsChill@users.noreply.github.com>
2020-12-26 08:13:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from discord.ext import commands
 from discord.utils import get
 
 intents = discord.Intents().all()
-client = commands.AutoShardedBot(command_prefix=":", intents=intents, help_command=None)
+client = commands.AutoShardedBot(command_prefix="-", intents=intents, help_command=None)
 
 @client.event
 async def on_member_join(member):
