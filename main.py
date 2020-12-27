9134c847e0e3fb5fbf77a32ad9664a42a6a317cb
LetsChill <74666116+LetsChill@users.noreply.github.com>
2020-12-27 06:42:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -35,4 +35,5 @@ for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
        client.load_extension(f'cogs.{filename[:-3]}')
 
+status.start()
 client.run("NzcyODk0NzQxNTgyNzA4Nzc4.X6BUUg.5CXfanDNexURwqbjJwS-sdtWyNc")
