fc0d1692c69fab8b795a327e211b75a204ebbbf6
LetsChill <74666116+LetsChill@users.noreply.github.com>
2020-12-30 10:58:57 +0200

    Update logging.py

--- cogs/logging.py
+++ cogs/logging.py
@@ -29,11 +29,10 @@ class Logging(commands.Cog):
       timestamp=after.created_at,
       description = "message was edited", color=0x982abc
       ) 
-      embed.set_author(name=f'{before.author.name} {before.author.discriminator}', icon_url=before.author.avatar_url)
-      embed.set_footer(text=f"Author ID:{before.author.id} • Message ID: {before.id}")
-      embed.add_field(name=f'User: ', value=f'{before.author.mention}', inline=False)
-      embed.add_field(name='Before:', value=f"{before.content}", inline=False)
-      embed.add_field(name="After:", value=f"{after.content}", inline=False)
+      embed.set_author(name=f"Subaru Logging")
+      embed.add_field(name=f'User: ', value=f'{before.author.mention}\n', inline=False)
+      embed.add_field(name='Original:', value=f"**{before.content}**\n", inline=False)
+      embed.add_field(name="Edited:", value=f"**{after.content}**", inline=False)
       channel = discord.utils.get(guild.channels, name='message-logs')
       await channel.send(embed=embed)
 
