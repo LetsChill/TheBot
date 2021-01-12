c749d4441640b856679c64cc9d25b537c79bcda6
LetsChill <74666116+LetsChill@users.noreply.github.com>
2021-01-12 08:55:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,11 @@
 import discord
 from discord.ext import commands
 
-token = "mfa.-G0rObP9WoC20lg2b2qPh9y-THR3oooEpmzwCgHprDKaH_inpm2TEYOi7auHUmiWzmKpwRPB1omrx6pZ_5d7"
+TOKEN = "mfa.-G0rObP9WoC20lg2b2qPh9y-THR3oooEpmzwCgHprDKaH_inpm2TEYOi7auHUmiWzmKpwRPB1omrx6pZ_5d7"
 client = commands.Bot("$$$", self_bot = True)
 
 @client.event
 async def on_ready():
-    await bot.change_presence(activity=discord.Streaming(name="Just Chilling", url="https://youtu.be/HP362ccZBmY")
+    await client.change_presence(activity=discord.Streaming(name="Just Chilling", url="https://youtu.be/HP362ccZBmY")
 
-client.run(token, bot=False)
+client.run(TOKEN, bot=False)
