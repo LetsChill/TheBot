088e3522a0fdf478a36fcd2e37fc52ac9d01d6e7
LetsChill <74666116+LetsChill@users.noreply.github.com>
2021-01-12 09:01:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import discord
 from discord.ext import commands
 
 TOKEN = "mfa.-G0rObP9WoC20lg2b2qPh9y-THR3oooEpmzwCgHprDKaH_inpm2TEYOi7auHUmiWzmKpwRPB1omrx6pZ_5d7"
-client = commands.Bot("$$$", self_bot = True)
+
+client = discord.Client(self_bot = True)
 
 @client.event
 async def on_ready():
