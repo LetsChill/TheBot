15bda62fb9b3aa1a9898affdbe9f13c090cd9ff4
LetsChill <74666116+LetsChill@users.noreply.github.com>
2021-01-17 19:40:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ client = discord.Client(self_bot = True)
 
 @client.event
 async def on_ready():
-  await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="Kimi No Na Wa | Your name | Half way there"))
+  await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="Kimi No Na Wa | Your name | Half way there"))
 
 client.run(TOKEN, bot=False)
