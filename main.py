95d6c1ea18553feff38d6b4146ba867370ef9ceb
LetsChill <74666116+LetsChill@users.noreply.github.com>
2021-01-17 19:36:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ client = discord.Client(self_bot = True)
 
 @client.event
 async def on_ready():
-    await client.change_presence(activity=discord.Streaming(name = "Just Chilling", url = "https://youtu.be/HP362ccZBmY"))
+  await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="Kimi No Na Wa | Your name | Half way there"))
 
 client.run(TOKEN, bot=False)
