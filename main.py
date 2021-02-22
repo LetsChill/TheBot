542f0a455afa0972c26e81250807781d4a33069f
TahaMori <74666116+LetsChill@users.noreply.github.com>
2021-02-22 18:03:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,13 @@ from discord.ext import commands
 
 TOKEN = "TOKEN"
 
-client = discord.Client()
+client = commands.AutoShardedBot(command_prefix="?")
+
+#cogs loader
+for filename in os.listdir('./cogs'):
+        if filename.endswith(".py") and not filename.startswith("_"):
+            bot.load_extension(f"cogs.{filename[:-3]}")
+            print("Cogs Loaded!")
+
 
 client.run(TOKEN)
